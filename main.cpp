9498f8fa639f75165320cd0df53abaf870625310
Adam-Torek <adamtorek@u.boisestate.edu>
2024-02-09 00:30:59 -0700

    continued working on linked list implementation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <stdexcept>
 
 template<typename U> class DoubleLinkedList {
         private:
@@ -19,6 +19,13 @@ template<typename U> class DoubleLinkedList {
                         n->data = d;
                         n->next = n->prev = NULL;
                         return n;
+                }
+
+                void insertMiddleNode(Node *n, Node *current) {
+                        n->prev = current;
+                        n->next = current->next;
+                        current->next->prev = n;
+                        current->next = n;
                 }
         public: 
                 DoubleLinkedList() {
@@ -49,12 +56,12 @@ template<typename U> class DoubleLinkedList {
                                 n->prev = tail;
                                 tail = n;
                         }
+                        size++;
                 }
 
                 void add(int index, U *d) {
                         if(index < 0 || index > size) {
-                                throw std::exception("Passed index " + std::to_string(index) + " is out of bounds. The index must be greater"
-                                + "than 0 and less than " + std::to_string(size) + ".");
+                                throw std::invalid_argument("Index out of bounds");
                         }
 
                         if(index == 0) {
@@ -66,15 +73,50 @@ template<typename U> class DoubleLinkedList {
                         else {
                                 Node *n = newNode(d);
                                 Node *current = head;
-                                for(int i = 0; i < index; i++) {
+                                for(int i = 0; i < index-1; i++) {
                                         current = current->next;
                                 }
-                                n->prev = current;
-                                n->next = current->next;
-                                current->next->prev = n;
-                                current->next = n;
+                                insertMiddleNode(n, current);
+                                size++;
                         }
                        
+                }
+
+                void add(U* target, U* d) {
+
+                        if(size == 0) {
+                                throw std::exception("Empty List");
+                        }
+
+                        if(target == tail->data) {
+                                addLast(d);
+                        }
+
+                        else {
+                                Node *current = head;
+
+                                while(current != NULL && current->data != target) {
+                                        current = current->next;
+                                }
+
+                                if(current == NULL) {
+                                        return;
+                                }
+                                insertMiddleNode(newNode(d), current);
+                                size++;
+                        }
+                }
+
+                U* get(int index) {
+                        if(index < 0 || index >= size) {
+                                throw std::invalid_argument("Index out of Bounds");
+                        }
+
+                        Node *current = head;
+                        for(int i = 0; i < size-1; i++) {
+                                current = current->next;
+                        }
+                        return current->data;
                 }
 
 };
@@ -87,5 +129,7 @@ int main() {
         int *ptr2 = &j;
         DoubleLinkedList<int> *list = new DoubleLinkedList<int>();
         list->addFirst(ptr);
+        list->add(1, ptr2);
+        std::cout << "Value stored in list at index 1 is " <<  *list->get(1);
         return 0;
 }

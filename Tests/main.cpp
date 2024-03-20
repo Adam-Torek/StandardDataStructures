a8216021b90b397f85fceeaa53d7a64054ac8905
Adam-Torek <adamtorek@u.boisestate.edu>
2024-03-19 21:53:33 -0600

    worked more on empty array unit tests

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -4,12 +4,15 @@
 
 TEST(TestArray, TestEmptyArray) {
     ds::linear::array<int, 0> arr;
+
     EXPECT_EQ(arr.size(), 0);
     EXPECT_EQ(arr.max_size(), 0);
     EXPECT_EQ(arr.empty(), true);
     EXPECT_NE(arr.front(), 0);
     EXPECT_NE(arr.back(), 0);
     EXPECT_EQ(arr.front(), arr.back());
+    EXPECT_NE(*arr.data(), 0);
+
     EXPECT_EQ(arr.begin(), arr.end());
     EXPECT_EQ(arr.cbegin(), arr.cend());
     EXPECT_NE(arr.rbegin(), arr.rend());

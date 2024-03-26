2587d3d52b964573c773dc8aba3ab58402377e8e
Adam-Torek <adamtorek@u.boisestate.edu>
2024-03-25 21:09:43 -0600

    added a lot of different 1 element array unit tests

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -10,7 +10,9 @@ static void run_empty_array_tests(ds::linear::static_array<int, 0> arr) {
     EXPECT_NE(arr.front(), 0);
     EXPECT_NE(arr.back(), 0);
     EXPECT_EQ(arr.front(), arr.back());
-    //EXPECT_NE(*arr.data(), 0);
+    EXPECT_NE(arr.at(0), 0);
+    EXPECT_NE(arr[0], 0);
+    EXPECT_NE(*arr.data(), 0);
 
     EXPECT_EQ(arr.begin(), arr.end());
     EXPECT_EQ(arr.cbegin(), arr.cend());
@@ -37,10 +39,32 @@ TEST(TestArray, TestEmptyArrayInitializedAnotherArray) {
     run_empty_array_tests(empty_arr2);
 }
 
-static void run_one_element_tests(ds::linear::static_array<int, 1> arr, std::array<int, 1> other) {
-    EXPECT_EQ(arr.size(), 1);
-    EXPECT_EQ(arr.max_size(), 1);
+template<unsigned int SIZE>
+static void run_n_element_tests(ds::linear::static_array<int, SIZE> arr, std::array<int, SIZE> other) {
+    EXPECT_EQ(arr.size(), SIZE);
+    EXPECT_EQ(arr.max_size(), SIZE);
     EXPECT_EQ(arr.front(), other.front());
     EXPECT_EQ(arr.back(), other.back());
+    int* arr_data = arr.data();
+    int* other_data = other.data();
+    typename std::array<int, SIZE>::iterator other_iter = other.begin();
+    ds::linear::array_iterator<ds::linear::static_array<int, SIZE>> arr_iter = arr.begin();
+    for(unsigned int i = 0; i < SIZE; i++) {
+        EXPECT_EQ(other_iter[i], arr_iter[i]);
+        EXPECT_EQ(arr.at(i), other.at(i));
+        EXPECT_EQ(arr[i], other[i]);
+        EXPECT_EQ(arr_data[i], other_data[i]);
+    }
+
+    while(arr_iter != arr.end()) {
+        EXPECT_EQ(*arr_iter, *other_iter);
+        arr_iter++, other_iter++;
+    }
+}
+
+TEST(TestArray, TestOneElementListInitialized) {
+    ds::linear::static_array<int, 1> arr = {1};
+    std::array<int, 1> other = {1};
+    run_n_element_tests<1>(arr, other);
 }
 
--- build/Tester[1]_tests.cmake
+++ build/Tester[1]_tests.cmake
@@ -4,4 +4,6 @@ add_test( TestArray.TestEmptyArrayInitialized /home/adam-torek/StandardDataStruc
 set_tests_properties( TestArray.TestEmptyArrayInitialized PROPERTIES WORKING_DIRECTORY /home/adam-torek/StandardDataStructures/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
 add_test( TestArray.TestEmptyArrayInitializedAnotherArray /home/adam-torek/StandardDataStructures/build/Tester [==[--gtest_filter=TestArray.TestEmptyArrayInitializedAnotherArray]==] --gtest_also_run_disabled_tests)
 set_tests_properties( TestArray.TestEmptyArrayInitializedAnotherArray PROPERTIES WORKING_DIRECTORY /home/adam-torek/StandardDataStructures/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( Tester_TESTS TestArray.TestEmptyArray TestArray.TestEmptyArrayInitialized TestArray.TestEmptyArrayInitializedAnotherArray)
+add_test( TestArray.TestOneElementListInitialized /home/adam-torek/StandardDataStructures/build/Tester [==[--gtest_filter=TestArray.TestOneElementListInitialized]==] --gtest_also_run_disabled_tests)
+set_tests_properties( TestArray.TestOneElementListInitialized PROPERTIES WORKING_DIRECTORY /home/adam-torek/StandardDataStructures/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set( Tester_TESTS TestArray.TestEmptyArray TestArray.TestEmptyArrayInitialized TestArray.TestEmptyArrayInitializedAnotherArray TestArray.TestOneElementListInitialized)
